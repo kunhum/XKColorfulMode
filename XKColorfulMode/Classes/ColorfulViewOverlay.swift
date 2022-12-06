fb93b04484ef7623281cbf81b432f7d8f26fa453
xk <weikun.xie@get88.cn>
2022-12-06 13:40:25 +0800

    初始化

---
+++ XKColorfulMode/Classes/ColorfulViewOverlay.swift
@@ -0,0 +1,20 @@
+//
+//  ColorfulViewOverlay.swift
+//  Pods-XKColorfulMode_Example
+//
+//  Created by kenneth on 2022/12/6.
+//
+
+import UIKit
+
+class ColorfulViewOverlay: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
